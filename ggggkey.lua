44ef9538b43a44f2a28fb6f86be231b07a3a0774
Sekayzee <141709850+Skyzee02@users.noreply.github.com>
2025-10-25 11:55:33 +0700

    ggggkey.lua

--- ggggkey.lua
+++ ggggkey.lua
@@ -1,9 +1,11 @@
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
+
 local Window = Rayfield:CreateWindow({
    Name = "Rayfield Example Window",
    Icon = 0, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
    LoadingTitle = "Sekay Hub",
    LoadingSubtitle = "by Sekayzee",
-   ShowText = "Sekay Hub", -- for mobile users to unhide rayfield, change if you'd like
+   ShowText = "Rayfield", -- for mobile users to unhide rayfield, change if you'd like
    Theme = "Default", -- Check https://docs.sirius.menu/rayfield/configuration/themes
 
    ToggleUIKeybind = "K", -- The keybind to toggle the UI visibility (string like "K" or Enum.KeyCode)
